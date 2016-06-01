bca39ee6c32377caef23e03e599a7ce9d16b4efe
Pendy <pendy@nakroshis.phy.usm.maine.edu>
2016-06-01 12:35:26 -0400

    Formatting changes
    Updated output formatting to use regular expressions instead of
    assuming a fixed message length. This allowed us to consolidate the
    port writes and formatting into a new class function ‘writeToPort’
    which gets used by the current and voltage functions.

--- powersupply2/powersupply2.py
+++ powersupply2/powersupply2.py
@@ -1,5 +1,6 @@
 import time
 import serial
+import re
 
 class PowerSupply():
 
@@ -7,6 +8,7 @@ class PowerSupply():
         
         self.portAddress = portAddress # the string that tells the self.serial library to open the correct port
         self.portOpen = False
+        self.output = 
         
 
     def openPort(self):
@@ -39,13 +41,29 @@ class PowerSupply():
         while self.ser.inWaiting() > 0: # while we have bytes wating in the buffer to read
             # detect the stop bit from the powersupply which happens to be '\x11' the start bit is '\x13' which I use for testing if this catch works
             bufferByte = self.ser.read() # pull a byte off the input buffer
-            if bufferByte == (b'\x11'): # if we get a stop char from the powersupply
-                self.ser.reset_input_buffer() # clear the buffer
-                break # break out of the while loop
+            # messages always start with '\x13' so we can wait for that char and parse out the message
+            if bufferByte == (b'\x13'): # if we are at the begining of a message, 
+                bufferByte = self.ser.read() # read past the start bit
+                while bufferByte != (b'\x11') and bufferByte != (b'\r'): # while we are inside the message
+                    out += bufferByte
+                    bufferByte = self.ser.read() # read the next byte
             
-            out += bufferByte # add them one at a time to the output
-            
+  
+            self.ser.reset_input_buffer() # clean out the input buffer after reading the message
+        out = out.decode("utf-8") # switch from byte array to string
+        out = self.formatOutput(out)
+        
         return out
+        
+    def formatOutput(self, stuffToFormat):
+        # parse out the string
+        temp = re.search(r'\d+\.\d', stuffToFormat)
+        if bool(temp):
+            return float(temp.group())
+        else:
+            return stuffToFormat
+            
+         
         
 
     def voltage(self, setVoltage = None):
@@ -53,6 +71,9 @@ class PowerSupply():
         If no arguments are passed, this function queries the voltage and returns it.
         otherwise it will try to set the voltage of the powersupply and return a None type. 
         '''
+        
+        if(self.writeToPort(bytearray('STAT?', 'utf-8')) != 'CV'):
+             raise Exception('Incorrect mode! Supply is in constant current mode.')
         # the voltage value that the powersupply returns 
         getVoltage = None # will be none if we are setting the voltage
         out = None
@@ -63,7 +84,7 @@ class PowerSupply():
 
         if setVoltage: # if we pass a value other than none, try to set the voltage
             voltage=str('%05.2f' % voltage) # formats the voltage to have 5 chars and 2 digits before and after the decimal
-            out = self.writeToPort(bytearray('VOLT ' + voltage, 'utf-8')) #format and write to the port
+            out = self.writeToPort(bytearray('VOLT ' + voltage, 'utf-8')) #format and write to the port powersupply will return a start and stop bit but no info if sucessfull
         else: # querry the voltage from the powersupply.
             out = self.writeToPort(bytearray('VOLT?', 'utf-8')) # write command to 'get voltage' to the serial port. command is 'VOLT?' 
 
@@ -76,7 +97,7 @@ class PowerSupply():
         #return(getVoltage) # return the voltage (None if setting, float in volts if getting)
         return(str(out))
         
-    def current(self, setCurrent = None):
+    def current(self, setCurrent = 'None'):
         '''
         If no arguments are passed this function queries the current and returns it.
         otherwise it will try to set the current of the powersupply and return a None type.
@@ -87,6 +108,12 @@ class PowerSupply():
         
         The returned current float is also in milliamps.
         '''
+        
+        if(self.writeToPort(bytearray('STAT?', 'utf-8')) != 'CC'):
+            #print('error')
+            raise Exception('Incorrect mode! Supply is in constant voltage mode.')
+             
+             
 
         getCurrent = None # the current the powersupply returns 
         out = None
@@ -94,14 +121,17 @@ class PowerSupply():
         initialPortState = self.portOpen # handles the port state so we can leave it open if it is open 
         if initialPortState == False:
             self.openPort()
-
-        if setCurrent: # if we pass a value other than none, try to set the current
-            current=str('%05.1f' % setCurrent) # sets the current to have 5 chars and 3 digits before and 1 digit after the decimal
-            print(current)
-            out = self.writeToPort(bytearray('CURR ' + current, 'utf-8')) #format and write to the port
+        
+        if setCurrent != 'None': # if we pass a value other than none, try to set the current
+            try:
+                current=str('%05.1f' % setCurrent) # sets the current to have 5 chars and 3 digits before and 1 digit after the decimal
+                print(current)
+                out = self.writeToPort(bytearray('CURR ' + current, 'utf-8')) #format and write to the port
+            except:
+                print('invalid thing')
         else: # get the current from the powersupply if we are not tring to set it
             out = self.writeToPort(bytearray('CURR?', 'utf-8'))
-            s = str(out[1:-2]) # truncate the output to remove stop and start bytes then cast as string
+            #s = str(out[1:-2]) # truncate the output to remove stop and start bytes then cast as string
             #getCurrent = (float(s[12:-4])) # strip the units and message from the output string and cast as a float
 
         if initialPortState == False and self.portOpen == True:

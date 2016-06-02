a6e611e8cafbfecabdbe4e243c8843cfba1d151e
WilliamMorse <pendy@nakroshis.phy.usm.maine.edu>
2016-06-02 12:08:28 -0400

    optimized function structure
    Changed the way errors are handled now we check fro them last
    
    Moved parsing for all commands that return text in the actual command
    function (avoids checks for each possible return in the writeToPort()
    function).

--- powersupply2/powersupply2.py
+++ powersupply2/powersupply2.py
@@ -54,28 +54,44 @@ class PowerSupply():
         # if we don't, then that means that we just set the current and the powersupply is 
         # just telling us that it's finished setting the current with the return bytes
         if len(out) == 0:
-            return(out)
+            return 
         
         out = out.decode("utf-8") # switch from type bytearray to string
         
         # now we want to format the output to be a float if we have a number.
-        temp = re.search(r'\d+\.\d', out) # look for a number in temp
+        temp = re.search(r'\d+\.\d', out) # look for a number in out
         if bool(temp): # if we find a numbers in the string, 
             out = float(temp.group()) # put them together and cast them as a float.
-        else: # if we don't have a number or a null, then we have gotten a text command back or an error!
-            if out == 'CC' or out == 'CV': # numeric commands
-                return(out)
-            elif out == 'Syntax Error':
-                print('Syntax Error. Command not a number or value too high for the power supply')
-                #raise Exception('Syntax Error. Command not a number or value too high for the power supply')
-            elif out == 'Out Of Range':
-                print('Input number out of range! (Likly too small. Minimum is 0.01V or 0.1mA)')
-                #raise Exception('Input number out of range! (Likly too small. Minimum is 0.01V or 0.1mA)')
-            else:
-                print('partial message receved: %s' % out)
-                #raise Exception('partial message receved: %s' % out)
-        return(out)
-
+            return(out) # returns a float (current value or voltage value)
+        else: 
+            return(out) # returns a string (error message or text reply to command)
+        
+    def parseErrorMessages(self, message):
+        '''
+        Check the input message for any of the predefined error messages from the powersupply 
+        and raise an exception if an error is found.
+        '''
+        if message == 'Syntax Error':
+            print('Syntax Error. Command not a number or value too high for the power supply')
+            #raise Exception('Syntax Error. Command not a number or value too high for the power supply')
+        elif message == 'Out Of Range':
+            print('Input number out of range! (Likly too small. Minimum is 0.01V or 0.1mA)')
+            #raise Exception('Input number out of range! (Likly too small. Minimum is 0.01V or 0.1mA)')
+        else:
+            print('strange message receved: %s' % out)
+            #raise Exception('strange message receved: %s' % out)
+            
+    def checkMode(self):
+        '''
+        Test to see if the supply is in constant current mode or constant voltage mode
+        returns a string 'CV' if constant voltage and 'CC' if the mode is constant current. 
+        '''
+        out = self.writeToPort(bytearray('STAT?', 'utf-8')
+        if out == 'CV' or out == 'CC': # if we get expected values for the mode
+            return out # return them!
+        else:
+            self.parseErrorMessages(out) # check the errors 
+            raise Exception('enable the exceptions in the parseErrorMessages functon!')
 
     def voltage(self, setVoltage = 'None'):
         '''
@@ -83,7 +99,7 @@ class PowerSupply():
         otherwise it will try to set the voltage of the powersupply and return a None type. 
         '''
         
-        if(self.writeToPort(bytearray('STAT?', 'utf-8')) != 'CV'):
+        if(self.checkMode()) != 'CV'): # check the mode of the powersupply
              raise Exception('Incorrect mode! Supply is in constant current mode.')
 
         initialPortOpen = self.portOpen # handles the port state so we can leave it open if it is already open.
@@ -92,17 +108,22 @@ class PowerSupply():
 
         if setVoltage != 'None': # if we pass a value other than none, try to set the voltage
             voltage=str('%05.2f' % setVoltage) # formats the voltage to have 5 chars and 2 digits before and after the decimal
-            self.writeToPort(bytearray('VOLT ' + voltage, 'utf-8')) #format and write to the port
-            out = None
-            
+            out = self.writeToPort(bytearray('VOLT ' + voltage, 'utf-8')) #format and write to the port
+            if len(out) != 0: # if we get anything back
+                self.parseErrorMessages(out) # analize errors
+                raise Exception('enable the exceptions in the parseErrorMessages functon!')
+                
         else: # querry the voltage from the powersupply.
             out = self.writeToPort(bytearray('VOLT?', 'utf-8')) # write command to query current powersupply voltage
-            
+            if type(out) != float: # did we get a number?
+                elf.parseErrorMessages(out) # analize errors
+                raise Exception('enable the exceptions in the parseErrorMessages functon!')
+                
         if initialPortOpen == False and self.portOpen == True: # leave the port how we found it. 
             self.closePort() # if it was open before the call, keep it open. Else close it.
             
         # return the voltage (None if setting, float in volts if getting)
-        return(str(out))
+        return(out)
         
     def current(self, setCurrent = 'None'):
         '''
@@ -116,8 +137,7 @@ class PowerSupply():
         The returned current float is also in milliamps.
         '''
         
-        if(self.writeToPort(bytearray('STAT?', 'utf-8')) != 'CC'):
-            #print('error')
+        if(self.checkMode() != 'CC'): # make sure the supply is in constant current mode. 
             raise Exception('Incorrect mode! Supply is in constant voltage mode.')
 
         initialPortState = self.portOpen # handles the port state so we can leave it open if it is open 
@@ -125,16 +145,21 @@ class PowerSupply():
             self.openPort()
         
         if setCurrent != 'None': # if we pass a value other than none, try to set the current    
-            current=str('%05.1f' % setCurrent) # sets the current to have 5 chars and 3 digits before and 1 digit after the decimal
-            self.writeToPort(bytearray('CURR ' + current, 'utf-8')) #format and write to the port
-            out = None
+            current=str('%05.1f' % setCurrent) # sets the setCurrent to have 5 chars and 3 digits before and 1 digit after the decimal
+            out = self.writeToPort(bytearray('CURR ' + current, 'utf-8')) #format and write to the port
+            if len(out) != 0: # if we get anything back
+                self.parseErrorMessages(out) # analize errors
+                raise Exception('enable the exceptions in the parseErrorMessages functon!')
             
         else: # get the current current from the powersupply
             out = self.writeToPort(bytearray('CURR?', 'utf-8'))
+            if type(out) != float:
+                elf.parseErrorMessages(out) # analize errors
+                raise Exception('enable the exceptions in the parseErrorMessages functon!')
             
         if initialPortState == False and self.portOpen == True:
             self.closePort()
             
         # return the current (type is None if setting the current and float in milliamps if getting the voltage)
-        return(str(out))
+        return(out)
         
